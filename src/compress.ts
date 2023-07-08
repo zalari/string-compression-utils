edd409fb2f97b4360d4f3dcdd4c78469665e98ea
David Enke <david.enke@zalari.de>
2023-07-08 15:36:03 +0200

    chore: align to lint warnings

--- src/compress.ts
+++ src/compress.ts
@@ -1,9 +1,9 @@
 /**
  * Compress a string with browser native APIs into a binary string representation
  *
- * @param data Input string that should be compressed
- * @param encoding Compression algorithm to use
- * @return The compressed binary string
+ * @param data - Input string that should be compressed
+ * @param encoding - Compression algorithm to use
+ * @returns The compressed binary string
  */
 export async function compressRaw(data: string, encoding: CompressionFormat): Promise<string> {
   // stream the string through the compressor
@@ -19,9 +19,9 @@ export async function compressRaw(data: string, encoding: CompressionFormat): Pr
 /**
  *	Compress a string with browser native APIs into a string representation
  *
- * @param data Input string that should be compressed
- * @param encoding Compression algorithm to use
- * @return The compressed string
+ * @param data - Input string that should be compressed
+ * @param encoding - Compression algorithm to use
+ * @returns The compressed string
  */
 export async function compress(data: string, encoding: CompressionFormat): Promise<string> {
   return btoa(await compressRaw(data, encoding));
--- src/decompress.ts
+++ src/decompress.ts
@@ -1,9 +1,9 @@
 /**
  * Decompress a binary string representation with browser native APIs in to a normal js string
  *
- * @param binary Binary string that should be decompressed, e.g. the output from `atob`
- * @param encoding Decompression algorithm to use
- * @return The decompressed string
+ * @param binary - Binary string that should be decompressed, e.g. the output from `atob`
+ * @param encoding - Decompression algorithm to use
+ * @returns The decompressed string
  */
 export async function decompressRaw(binary: string, encoding: CompressionFormat): Promise<string> {
   // stream the string through the decompressor
@@ -17,9 +17,9 @@ export async function decompressRaw(binary: string, encoding: CompressionFormat)
 /**
  * Decompress a string representation with browser native APIs in to a normal js string
  *
- * @param data String that should be decompressed
- * @param encoding Decompression algorithm to use
- * @return The decompressed string
+ * @param data - String that should be decompressed
+ * @param encoding - Decompression algorithm to use
+ * @returns The decompressed string
  */
 export async function decompress(data: string, encoding: CompressionFormat): Promise<string> {
   return decompressRaw(atob(data), encoding);
